47068399a941b5ab8d6333aa7ac5c7b67fc503ad
faiqazmi9 <faiqazmi9@gmail.com>
2021-02-22 23:59:57 +0700

    making toolbar custom collapsed

--- app/src/main/java/com/example/awesomeappjava/MainActivity.java
+++ app/src/main/java/com/example/awesomeappjava/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.awesomeappjava;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    MaterialToolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = findViewById(R.id.toolbar);
+
+        setupToolbar();
+    }
+
+    protected void setupToolbar() {
+        setSupportActionBar(mToolbar);
+        ActionBar mActionBar = this.getSupportActionBar();
+        assert mActionBar != null;
+        mToolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
+        mActionBar.setTitle("Nature");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_list, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_list:
+                finish();
+                return true;
+            case R.id.action_grid:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
