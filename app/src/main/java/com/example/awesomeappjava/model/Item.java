28ee5b6fb0a2a83784c216ef73c50855d3fd3a15
faiqazmi9 <faiqazmi9@gmail.com>
2021-02-23 01:41:34 +0700

    add description

--- app/src/main/java/com/example/awesomeappjava/model/Item.java
+++ app/src/main/java/com/example/awesomeappjava/model/Item.java
@@ -3,12 +3,12 @@ package com.example.awesomeappjava.model;
 public class Item {
     private int imgResId;
     private String title;
-    private int likes;
-    private int comments;
+    private String description;
 
-    public Item(int imgResId, String title) {
+    public Item(int imgResId, String title, String description) {
         this.imgResId = imgResId;
         this.title = title;
+        this.description = description;
     }
 
     public int getImgResId() {
@@ -17,5 +17,9 @@ public class Item {
 
     public String getTitle() {
         return title;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
