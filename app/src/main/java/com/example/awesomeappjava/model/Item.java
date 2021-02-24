f6c6e028cfea9f8114e436afd2b6e2e3bdaba1c1
faiqazmi9 <faiqazmi9@gmail.com>
2021-02-24 14:47:47 +0700

    add model for get api

--- app/src/main/java/com/example/awesomeappjava/model/Item.java
+++ app/src/main/java/com/example/awesomeappjava/model/Item.java
@@ -1,25 +1,102 @@
 package com.example.awesomeappjava.model;
 
 public class Item {
-    private int imgResId;
-    private String title;
-    private String description;
+    private int id;
+    private int width;
+    private int height;
+    private String url;
+    private String photographer;
+    private String photographerUrl;
+    private int photographerId;
+    private String avgColor;
+    private Src src;
 
-    public Item(int imgResId, String title, String description) {
-        this.imgResId = imgResId;
-        this.title = title;
-        this.description = description;
+    public Item() {
+
     }
 
-    public int getImgResId() {
-        return imgResId;
+    public Item(int id, int width, int height, String url, String photographer, String photographerUrl,
+                int photographerId, String avgColor, Src src) {
+        this.id = id;
+        this.width = width;
+        this.height = height;
+        this.url = url;
+        this.photographer = photographer;
+        this.photographerUrl = photographerUrl;
+        this.photographerId = photographerId;
+        this.avgColor = avgColor;
+        this.src = src;
     }
 
-    public String getTitle() {
-        return title;
+    public int getId() {
+        return id;
     }
 
-    public String getDescription() {
-        return description;
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getPhotographer() {
+        return photographer;
+    }
+
+    public void setPhotographer(String photographer) {
+        this.photographer = photographer;
+    }
+
+    public String getPhotographerUrl() {
+        return photographerUrl;
+    }
+
+    public void setPhotographerUrl(String photographerUrl) {
+        this.photographerUrl = photographerUrl;
+    }
+
+    public int getPhotographerId() {
+        return photographerId;
+    }
+
+    public void setPhotographerId(int photographerId) {
+        this.photographerId = photographerId;
+    }
+
+    public String getAvgColor() {
+        return avgColor;
+    }
+
+    public void setAvgColor(String avgColor) {
+        this.avgColor = avgColor;
+    }
+
+    public Src getSrc() {
+        return src;
+    }
+
+    public void setSrc(Src src) {
+        this.src = src;
     }
 }
