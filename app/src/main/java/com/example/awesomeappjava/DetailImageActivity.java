3336dfd96c05fdaa7a803603dc1aef4588d01013
faiqazmi9 <faiqazmi9@gmail.com>
2021-02-24 16:39:57 +0700

    dissplaying image on detail image activity

--- app/src/main/java/com/example/awesomeappjava/DetailImageActivity.java
+++ app/src/main/java/com/example/awesomeappjava/DetailImageActivity.java
@@ -7,9 +7,15 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
+
+import org.apache.commons.lang3.StringUtils;
+
+import timber.log.Timber;
+
 public class DetailImageActivity extends AppCompatActivity {
 
-    private int img = 0;
+    private String img = "";
     private String title = "";
     private String desc = "";
 
@@ -21,16 +27,31 @@ public class DetailImageActivity extends AppCompatActivity {
         Intent i = getIntent();
         Bundle bundle = i.getExtras();
         assert bundle != null;
-        img = bundle.getInt("img", 0);
+        img = bundle.getString("img", "0");
         title = bundle.getString("title", "");
-        desc = bundle.getString("desc", "");
 
         ImageView mIv = findViewById(R.id.image);
         TextView mTvTitle = findViewById(R.id.title);
         TextView mTvDescription = findViewById(R.id.description);
         ImageView mIvBack = findViewById(R.id.iv_back);
 
-        mIv.setImageResource(img);
+        if (StringUtils.isNotEmpty(img)) {
+            try {
+                Glide.with(this).load(img)
+                        .placeholder(R.drawable.ic_launcher_foreground)
+                        .into(mIv);
+            } catch (Exception ex) {
+                Timber.e(ex);
+            }
+        } else {
+            try {
+                Glide.with(this).load(R.drawable.ic_launcher_foreground)
+                        .placeholder(R.drawable.ic_launcher_foreground)
+                        .into(mIv);
+            } catch (Exception ex) {
+                Timber.e(ex);
+            }
+        }
         mTvTitle.setText(title);
         mTvDescription.setText(desc);
 
